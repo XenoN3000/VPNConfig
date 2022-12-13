cdf2d688d692a2b0db17fe029b8feb6c1eeb875b
XenoN <38420835+XenoN3000@users.noreply.github.com>
2022-12-13 18:03:59 +0330

    [#v0.0.0] -- some stuf added git add !

--- src/main.rs
+++ src/main.rs
@@ -3,30 +3,62 @@ mod database;
 
 use std::{env, fs};
 use std::env::{current_dir, current_exe};
+use std::f64::consts::E;
+use std::fmt::Debug;
 use std::path::Path;
 use rusqlite::{params, Connection, Result};
+use crate::Cmd::{Connect, Delete, Edit, Insert, Help, List, Version};
+
+enum Cmd {
+    Insert,
+    Edit,
+    Delete,
+    Connect,
+    Help,
+    List,
+    Version,
+}
+
+fn add_config() {}
+
+fn connect() {}
+
+fn edit() {}
 
 
 fn main() {
-
-
     let mut exe_dir = current_exe().unwrap();
     exe_dir.pop();
-    let data_dir = format!("{}/DATA",exe_dir.display());
+    let data_dir = format!("{}/DATA", exe_dir.display());
 
-    let db = database::DB::open(data_dir,"vpn_db.db3".to_string()).unwrap();
-
-    let vpn = vpnconf::VPNConfig::new("20vpn".to_string(), "Cisco".to_string(), "hosien".to_string(), "1234".to_string());
-
-
-    println!("{}", vpn);
-
+    let db = database::DB::open(data_dir, "vpn_db.db3".to_string()).unwrap();
 
     let args: Vec<String> = env::args().collect();
 
-    if args.len() > 1 {}
+    if args.len() < 2 {
+        println!("wrong input size !!!");
+        return;
+    }
 
-    for i in 1..args.len() {
-        println!("{}", args[i]);
+
+    let com = match args[1].as_str() {
+        "-a" => Insert,
+        "-c" => Connect,
+        "-e" => Edit,
+        "-d" => Delete,
+        "-l" => List,
+        "-h" | "--help" => Help,
+        "-v" | "--v" | "--version" => Version,
+
+        _ => Connect,
     };
+
+
+
+    match com {
+        Insert if args.len() > 1 => add_config(),
+        Connect if args.len() > 1 => connect(),
+        Edit if args.len() > 1 => edit(),
+        _ => println!("wrong input size or argument !!! ")
+    }
 }
